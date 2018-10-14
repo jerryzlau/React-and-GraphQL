e19cb328b3fac352a998276536bd51ee679fa40c
Jerry Lau <jerryzlau@gmail.com>
2018-10-13 19:06:58 -0700

    finished module 1

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const Home = props => {
+  return (
+    <div>
+      yooo
+    </div>
+  );
+};
+
+Home.propTypes = {
+  
+};
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Sell = props => {
+  return (
+    <div>
+      selling
+    </div>
+  );
+};
+
+export default Sell;
