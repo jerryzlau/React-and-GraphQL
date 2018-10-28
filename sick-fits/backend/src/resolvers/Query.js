3f180c2985e1590395ab6ac9e49a149b6d4e8d7e
Jerry Lau <jerryzlau@gmail.com>
2018-10-28 14:23:28 -0700

    set up item resolvers in backend

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,17 @@
-const Query = {};
+const {forwardTo} = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db'),
+  
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+  // dogs(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // return all dogs
+  //   return global.dogs;
+  // }
+};
 
 module.exports = Query;
