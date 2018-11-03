4d6a68ecf70abda933857d9753f0bd391e5c5d3b
Jerry Lau <jerryzlau@gmail.com>
2018-11-03 14:14:50 -0700

    created udpate functions for back and front end

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -8,14 +8,19 @@ const Mutations = {
     }, info);
 
     return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const updates = {...args};
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
   }
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   // create a dog
-  //   const newDog = {name: args.name};
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
+
 };
 
 module.exports = Mutations;
