efbafe76bda5095187655c2dd0c56ba99c993744
Jerry Lau <jerryzlau@gmail.com>
2018-11-03 11:44:48 -0700

    set up cloudinary image upload in createitem

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -45,7 +45,30 @@ class CreateItem extends Component {
     });
   }
 
+  uploadFile = async e => {
+    console.log('Uploading file...');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/jerryzlau/image/upload', 
+      {
+        method: 'POST',
+        body: data
+      });
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
+  }
+
   render() {
+    const {image} = this.state;
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} 
         variables={this.state}>
@@ -60,6 +83,17 @@ class CreateItem extends Component {
           }}>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input type="file" 
+                  id="file" 
+                  name="file" 
+                  className="file" 
+                  placeholder="File"
+                  onChange={this.uploadFile}
+                  required/>
+                  {image && <img width="200" src={image} alt="Upload Preview"/>}
+              </label>
               <label htmlFor="title">
                 Title
                 <input type="text" 
